46295240da719cf254124360d302bfc45460b10a
Brad Samuel <bradsamuel97@gmail.com>
2021-08-29 11:02:20 -0400

    successfully connected to db and port

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-    process.env.DEWY_DB_URI || 'mongodb://localhost/dewy',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
+const connectDB =  async ()=>{
+    try{
+        const conn = await mongoose.connect(process.env.DEWY_DB_URI)
+        .then(() => {
+            console.log(`MongoDB connection established successfully...`)
+        })
+    }catch(error){
+        console.error(`Error: ${error} `)
+        process.exit(1) 
     }
-);
+}
 
-module.exports = mongoose.connection;
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const db = require('./config/connection');
+const dotenv = require('dotenv');
+const connectDB = require('./config/connection');
 
+dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+connectDB();
+
+app.listen(PORT, () => console.log(`Server has been established on port ${PORT}`))
+
