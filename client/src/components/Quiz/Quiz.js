963da9281001ac2e3dc8058012b21b98b09f05b4
Brad Samuel <bradsamuel97@gmail.com>
2021-12-09 19:56:21 -0500

    Implemented next button and get results button in quiz

--- client/src/components/Quiz/Quiz.js
+++ client/src/components/Quiz/Quiz.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { questions } from "../../helpers/QuestionBank";
-import { Typography, Container, Box, StyledAvatar, Button } from "@mui/material";
+import {
+  Typography,
+  Container,
+  Box,
+  StyledAvatar,
+  Button,
+} from "@mui/material";
 import { useStyles } from "./styles";
 import FaceIcon from "@mui/icons-material/Face";
 import FaceRetouchingOffIcon from "@mui/icons-material/FaceRetouchingOff";
 import WaterIcon from "@mui/icons-material/Water";
 import SanitizerIcon from "@mui/icons-material/Sanitizer";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StyledAvatarTest = styled.div`
   display: flex;
@@ -19,49 +26,87 @@ const StyledAvatarTest = styled.div`
   transition: 0.2s all ease;
 
   &.one {
-    transform: ${props => props.question === 0 ? 'scale(1.1, 1.1)' : 'scale(1 , 1)'};
-    filter: ${props => props.question === 0 ? 'brightness(120%)' : 'brightness(100%)'};
+    transform: ${(props) =>
+      props.question === 0 ? "scale(1.1, 1.1)" : "scale(1 , 1)"};
+    filter: ${(props) =>
+      props.question === 0 ? "brightness(120%)" : "brightness(100%)"};
   }
   &.two {
-    transform: ${props => props.question === 1 ? 'scale(1.2, 1.2)' : 'scale(1 , 1)'};
-    filter: ${props => props.question === 1 ? 'brightness(120%)' : 'brightness(100%)'};
+    transform: ${(props) =>
+      props.question === 1 ? "scale(1.2, 1.2)" : "scale(1 , 1)"};
+    filter: ${(props) =>
+      props.question === 1 ? "brightness(120%)" : "brightness(100%)"};
   }
   &.three {
-    transform: ${props => props.question === 2 ? 'scale(1.2, 1.2)' : 'scale(1 , 1)'};
-    filter: ${props => props.question === 2 ? 'brightness(120%)' : 'brightness(100%)'};
+    transform: ${(props) =>
+      props.question === 2 ? "scale(1.2, 1.2)" : "scale(1 , 1)"};
+    filter: ${(props) =>
+      props.question === 2 ? "brightness(120%)" : "brightness(100%)"};
   }
   &.four {
-    transform: ${props => props.question === 3 ? 'scale(1.2, 1.2)' : 'scale(1 , 1)'};
-    filter: ${props => props.question === 3 ? 'brightness(120%)' : 'brightness(100%)'};
+    transform: ${(props) =>
+      props.question === 3 ? "scale(1.2, 1.2)" : "scale(1 , 1)"};
+    filter: ${(props) =>
+      props.question === 3 ? "brightness(120%)" : "brightness(100%)"};
   }
   &.five {
-    transform: ${props => props.question === 4 ? 'scale(1.2, 1.2)' : 'scale(1 , 1)'};
-    filter: ${props => props.question === 4 ? 'brightness(120%)' : 'brightness(100%)'};
+    transform: ${(props) =>
+      props.question === 4 ? "scale(1.2, 1.2)" : "scale(1 , 1)"};
+    filter: ${(props) =>
+      props.question === 4 ? "brightness(120%)" : "brightness(100%)"};
   }
-`
+`;
 
 const Quiz = () => {
-  const [currQuestion, setCurrQuestion] = useState(1);
+  const [currQuestion, setCurrQuestion] = useState(0);
   const [optionChosen, setoptionChosen] = useState("");
   const classes = useStyles();
+  const history = useHistory();
 
+  const nextQuestion = () => {
+    setCurrQuestion(currQuestion + 1);
+  };
+
+  const getResults = () => {
+    setCurrQuestion(currQuestion + 1);
+    history.push("/");
+  };
   return (
     <Container align="center">
       <Box className={classes.qNum}>
-        <StyledAvatarTest style={{ backgroundColor: "purple" }} className='one'
-        question={currQuestion}>
+        <StyledAvatarTest
+          style={{ backgroundColor: "purple" }}
+          className="one"
+          question={currQuestion}
+        >
           <FaceIcon />
         </StyledAvatarTest>
-        <StyledAvatarTest style={{ backgroundColor: "purple" }} className='two' question={currQuestion}>
+        <StyledAvatarTest
+          style={{ backgroundColor: "purple" }}
+          className="two"
+          question={currQuestion}
+        >
           <FaceRetouchingOffIcon />
         </StyledAvatarTest>
-        <StyledAvatarTest style={{ backgroundColor: "purple" }} className='three' question={currQuestion}>
+        <StyledAvatarTest
+          style={{ backgroundColor: "purple" }}
+          className="three"
+          question={currQuestion}
+        >
           <WaterIcon />
         </StyledAvatarTest>
-        <StyledAvatarTest style={{ backgroundColor: "purple" }} className='four' question={currQuestion}>
+        <StyledAvatarTest
+          style={{ backgroundColor: "purple" }}
+          className="four"
+          question={currQuestion}
+        >
           <SanitizerIcon />
         </StyledAvatarTest>
-        <StyledAvatarTest style={{ backgroundColor: "purple" }} className='five' question={currQuestion}>
+        <StyledAvatarTest
+          style={{ backgroundColor: "purple" }}
+          className="five"
+          question={currQuestion}
+        >
           <WbSunnyIcon />
         </StyledAvatarTest>
       </Box>
@@ -92,6 +137,15 @@ const Quiz = () => {
             {questions[currQuestion].optionD}
           </Button>
         ) : null}
+        {currQuestion == questions.length - 1 ? (
+          <Button onClick={getResults} variant="outlined">
+            Get Results
+          </Button>
+        ) : (
+          <Button onClick={nextQuestion} variant="outlined">
+            Next
+          </Button>
+        )}
       </Box>
     </Container>
   );
