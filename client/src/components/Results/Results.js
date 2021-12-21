a3ccc9c0220bf736c05afdda9222e9d5f355414c
Brad Samuel <bradsamuel97@gmail.com>
2021-12-20 20:57:55 -0500

    Deleted stepCard file and added the code to Results

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { Box, Container, Grid, Paper, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Grid,
+  Paper,
+  Typography,
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+} from "@mui/material";
 import { useStyles } from "./styles";
-import StepCard from "../StepCard/StepCard";
+import { blue } from "@mui/material/colors";
 
 const Results = () => {
-    const classes = useStyles();
+  const classes = useStyles();
   return (
-    <Container align='center'>
+    <Container align="center">
       <Box>
         <Typography variant="h2">Your Results</Typography>
       </Box>
@@ -16,22 +27,116 @@ const Results = () => {
         gridTemplateRows="repeat(5, 1fr)"
         gap={2}
       >
-        <Paper gridRow="span 8" elevated={12}>
-          <StepCard />
+        <Paper elevated={12}>
+          <Card>
+            <CardHeader
+              avatar={<Avatar sx={{ bgcolor: blue[500] }}>1</Avatar>}
+              title="First Step"
+              subheader="cleanse that thang!"
+            />
+            <CardMedia
+              component="img"
+              height="500"
+              image="https://images.pexels.com/photos/6621462/pexels-photo-6621462.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+            />
+            <CardContent>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                vel odio at tortor ornare vulputate. Phasellus pharetra orci at
+                dolor tempus, mollis dapibus mauris mollis. Praesent bibendum
+                tortor at gravida.
+              </Typography>
+            </CardContent>
+          </Card>
         </Paper>
-        <Paper gridRow="span 8" elevated={12}>
-          <StepCard />
+        <Paper  elevated={12}>
+          <Card>
+            <CardHeader
+              avatar={<Avatar sx={{ bgcolor: blue[500] }}>2</Avatar>}
+              title="First Step"
+              subheader="cleanse that thang!"
+            />
+            <CardMedia
+              component="img"
+              height="500"
+              image="https://images.pexels.com/photos/6621462/pexels-photo-6621462.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+            />
+            <CardContent>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                vel odio at tortor ornare vulputate. Phasellus pharetra orci at
+                dolor tempus, mollis dapibus mauris mollis. Praesent bibendum
+                tortor at gravida.
+              </Typography>
+            </CardContent>
+          </Card>
         </Paper>
-        <Paper gridRow="span 8" elevated={12}>
-          <StepCard />
+        <Paper  elevated={12}>
+          <Card>
+            <CardHeader
+              avatar={<Avatar sx={{ bgcolor: blue[500] }}>3</Avatar>}
+              title="First Step"
+              subheader="cleanse that thang!"
+            />
+            <CardMedia
+              component="img"
+              height="500"
+              image="https://images.pexels.com/photos/6621462/pexels-photo-6621462.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+            />
+            <CardContent>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                vel odio at tortor ornare vulputate. Phasellus pharetra orci at
+                dolor tempus, mollis dapibus mauris mollis. Praesent bibendum
+                tortor at gravida.
+              </Typography>
+            </CardContent>
+          </Card>
         </Paper>
-        <Paper gridRow="span 8" elevated={12}>
-          <StepCard />
+        <Paper elevated={12}>
+          <Card>
+            <CardHeader
+              avatar={<Avatar sx={{ bgcolor: blue[500] }}>4</Avatar>}
+              title="First Step"
+              subheader="cleanse that thang!"
+            />
+            <CardMedia
+              component="img"
+              height="500"
+              image="https://images.pexels.com/photos/6621462/pexels-photo-6621462.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+            />
+            <CardContent>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                vel odio at tortor ornare vulputate. Phasellus pharetra orci at
+                dolor tempus, mollis dapibus mauris mollis. Praesent bibendum
+                tortor at gravida.
+              </Typography>
+            </CardContent>
+          </Card>
         </Paper>
-        <Paper gridRow="span 8" elevated={12}>
-          <StepCard />
+        <Paper elevated={12}>
+          <Card>
+            <CardHeader
+              avatar={<Avatar sx={{ bgcolor: blue[500] }}>5</Avatar>}
+              title="First Step"
+              subheader="cleanse that thang!"
+            />
+            <CardMedia
+              component="img"
+              height="500"
+              image="https://images.pexels.com/photos/6621462/pexels-photo-6621462.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+            />
+            <CardContent>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                vel odio at tortor ornare vulputate. Phasellus pharetra orci at
+                dolor tempus, mollis dapibus mauris mollis. Praesent bibendum
+                tortor at gravida.
+              </Typography>
+            </CardContent>
+          </Card>
         </Paper>
-
       </Box>
     </Container>
   );
